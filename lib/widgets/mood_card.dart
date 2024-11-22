818fe725bb276ba9b23bc850021128fb825c3294
adyoprawira <adyoprawira@gmail.com>
2024-11-22 14:45:28 +0700

    Finished Tutorial 8

--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -1,5 +1,9 @@
+import 'package:mental_health_tracker/screens/login.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class ItemHomepage {
   final String name;
@@ -17,6 +21,7 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Material(
       // Specify the background color of the application theme.
       color: Theme.of(context).colorScheme.secondary,
@@ -25,7 +30,7 @@ class ItemCard extends StatelessWidget {
 
       child: InkWell(
         // Touch-responsive area
-        onTap: () {
+        onTap: () async {
           // Show SnackBar when clicked
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
@@ -40,6 +45,34 @@ class ItemCard extends StatelessWidget {
                 builder: (context) => const MoodEntryFormPage(),
               ),
             );
+          } else if (item.name == "View Mood") {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+            );
+          }
+          else if (item.name == "Logout") {
+              final response = await request.logout(
+                  "http://127.0.0.1:8000/auth/logout/");
+              String message = response["message"];
+              if (context.mounted) {
+                  if (response['status']) {
+                      String uname = response["username"];
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("$message Goodbye, $uname."),
+                      ));
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                  } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(message),
+                          ),
+                      );
+                  }
+              }
           }
         },
         // Container to store the Icon and Text
